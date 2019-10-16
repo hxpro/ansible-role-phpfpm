802b89df185ec8fab6b781e21469fa842f61bc88
Matěj Koudelka <matej@hxpro.cz>
2019-10-16 20:55:33 +0200

    use molecule for testing

---
+++ molecule/default/tests/test_default.py
@@ -0,0 +1,25 @@
+import os
+
+import testinfra.utils.ansible_runner
+
+testinfra_hosts = testinfra.utils.ansible_runner.AnsibleRunner(
+    os.environ['MOLECULE_INVENTORY_FILE']
+).get_hosts('all')
+
+
+def test_hosts_file(host):
+    f = host.file('/etc/hosts')
+
+    assert f.exists
+    assert f.user == 'root'
+    assert f.group == 'root'
+
+
+def test_phpfpm_running_and_enabled(host):
+    phpfpm = host.service("php70-php-fpm")
+    assert phpfpm.is_running
+    assert phpfpm.is_enabled
+
+
+def test_composer_installed(host):
+    assert host.find_command('composer')
